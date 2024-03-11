f32eb9d53de31778770ae09a7a9b77dd616641da
Gowtham-Rayapu <gowthamrayapu@lyrostech.com>
2024-03-11 11:44:44 +0530

    modified changes

---
+++ src/App2.js
@@ -0,0 +1,15 @@
+import Reactfrom from "react";
+import { ReactDOM } from "react-dom/client";
+import "./index.css";
+import App2 from "./App2";
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <App2 user=""/>
+        else{
+            return <Home UserName={props.user}/>;
+        }
+    </React.StrictMode>
+)
