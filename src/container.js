4213a6e66d3751e465f1aa1055a471462ec17580
Gowtham-Rayapu <gowthamrayapu@lyrostech.com>
2024-03-11 11:47:15 +0530

    modified changes

--- src/container.js
+++ src/container.js
@@ -2,6 +2,6 @@ import React from "react";
 import feeds from "./feeds"
 import post from "./post"
 export default function Container(props) {
-  if (props.flag === 1) return <Feeds />;
+  if (props.flag === 1) return <feeds />;
   else if (props.flag === 2) return <post />;
 }
